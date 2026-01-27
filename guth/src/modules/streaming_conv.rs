b11f2e9d5e9979486a91fedc64e18ea3c495678b
Sean Gallagher <stgallag@gmail.com>
2026-01-27 10:03:39 -0500

    Wire streaming state into conv modules

--- guth/src/modules/streaming_conv.rs
+++ guth/src/modules/streaming_conv.rs
@@ -1,5 +1,44 @@
+use crate::state::{StreamStep, StreamingModule};
+
 #[derive(Debug, Default)]
 pub struct StreamingConv1d;
 
 #[derive(Debug, Default)]
 pub struct StreamingConvTranspose1d;
+
+#[derive(Debug, Clone)]
+pub struct StreamingConvState {
+    pub step: StreamStep,
+}
+
+impl Default for StreamingConvState {
+    fn default() -> Self {
+        Self {
+            step: StreamStep::new(),
+        }
+    }
+}
+
+impl StreamingModule for StreamingConv1d {
+    type State = StreamingConvState;
+
+    fn init_state(&self, _batch_size: usize, _sequence_length: usize) -> Self::State {
+        StreamingConvState::default()
+    }
+
+    fn increment_step(&self, state: &mut Self::State, increment: usize) {
+        state.step.increment(increment);
+    }
+}
+
+impl StreamingModule for StreamingConvTranspose1d {
+    type State = StreamingConvState;
+
+    fn init_state(&self, _batch_size: usize, _sequence_length: usize) -> Self::State {
+        StreamingConvState::default()
+    }
+
+    fn increment_step(&self, state: &mut Self::State, increment: usize) {
+        state.step.increment(increment);
+    }
+}
